4a71287f3162f046ce82f5429a289c7fb8e58088
Cheng Chua <chuaccheng@gmail.com>
2019-08-20 11:49:21 +1000

    Add files via upload

--- Assignment1.R
+++ Assignment1.R
@@ -3,6 +3,7 @@
 # https://www.data-to-viz.com/graph/chord.html
 # http://stat.data.abs.gov.au/Index.aspx?DataSetCode=ABS_DEM_QIM
 
+# Gu, Z. (2014) circlize implements and enhances circular visualization in R. Bioinformatics. DOI: 10.1093/bioinformatics/btu393
 
 library(tidyverse)
 library(viridis)
@@ -25,15 +26,16 @@ data_long <- d %>%
   rownames_to_column %>%
   gather(key = 'key', value = 'value', -rowname)
 
+pdf("data_in_pdf.pdf", height = 11.69, width =  8.27)
 # parameters
-par(mar=c(0,1,2,1)+0.1) 
+
+par(mar=c(0,0,0,0)+0.1) 
 circos.clear()
 circos.par(start.degree = 90, gap.degree = 4, track.margin = c(-0.1, 0.1), points.overflow.warning = FALSE)
 
 
 grid.col = c("NSW" = "#0c2c84","VIC" = "#225ea8","SA" = "#1d91c0","TAS" = "#41b6c4",
             "WA" = "#ffe4af","NT" = "#FFC0C1","ACT" = "#FC8084","QLD" = "#93003a")
-
 
 
 # Base plot
@@ -60,15 +62,19 @@ circos.track(track.index = 1, panel.fun = function(x, y) {
 }, bg.border = NA)
 
 # adding on a title
-par(mar=c(0,0,6,0)+0.1) 
-title("Interstate Migration in Australia")
+par(mar=c(0,0,4,0))
 
+title("Interstate Migration in Australia")
+#title("Interstate Migration in Australia", sub = "sub title",
+#      cex.main = 2,   font.main= 3, col.main= "Black",
+#      cex.sub = 0.75, font.sub = 3, col.sub = "red")
   
 # adding a legend
-legend("bottomleft", inset = .03, legend = c("New South Wales (NSW)", "Victoria (VIC)", "South Australia (SA)", "Tasmania (TAS)", 
+legend("bottomleft", inset = .02, bg="transparent", legend = c("New South Wales", "Victoria (VIC)", "South Australia (SA)", "Tasmania (TAS)", 
 "Western Australia (WA)","Northern Territory (NT)","Australian Capital Territory (ACT)", "Queensland (QLD)"),
-       fill = c("#0c2c84","#225ea8","#1d91c0","#41b6c4","#ffe4af","#FFC0C1","#FC8084","#93003a"), cex=0.6, box.lty=0)
+       fill = c("#0c2c84","#225ea8","#1d91c0","#41b6c4","#ffe4af","#FFC0C1","#FC8084","#93003a"), cex=0.7, box.lty=0)
 
 
+dev.off()
 
 
